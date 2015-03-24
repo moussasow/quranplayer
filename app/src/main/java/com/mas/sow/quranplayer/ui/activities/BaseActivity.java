b8761c095dc74ceee2bf0bca397732885ce8c776
moussasow <sow.study@gmail.com>
2015-03-25 08:34:39 +0900

    first commit

---
+++ app/src/main/java/com/mas/sow/quranplayer/ui/activities/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.mas.sow.quranplayer.ui.activities;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Sow on 2015/03/08.
+ */
+public class BaseActivity extends ActionBarActivity {
+}
