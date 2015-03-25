af4103fcca8c6203c6bc7dda0c581804d096a612
moussasow <sow.study@gmail.com>
2015-03-25 22:39:04 +0900

    add application class

---
+++ app/src/main/java/com/mas/sow/quranplayer/application/QuranPlayerApplication.java
@@ -0,0 +1,9 @@
+package com.mas.sow.quranplayer.application;
+
+import android.app.Application;
+
+/**
+ * Created by Sow on 2015/03/25.
+ */
+public class QuranPlayerApplication extends Application {
+}
