6af8258894b3a6a613a691d86a1b7415ab14dd8b
Moussa Sow <sow.study@gmail.com>
2015-03-31 20:40:10 +0900

    Added Toolbar

--- app/src/main/java/com/mas/sow/quranplayer/application/QuranPlayerApplication.java
+++ app/src/main/java/com/mas/sow/quranplayer/application/QuranPlayerApplication.java
@@ -6,4 +6,16 @@ import android.app.Application;
  * Created by Sow on 2015/03/25.
  */
 public class QuranPlayerApplication extends Application {
+
+    private static QuranPlayerApplication sInstance;
+
+    public static QuranPlayerApplication getsInstance(){
+        return sInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+    }
 }
--- app/src/main/java/com/mas/sow/quranplayer/ui/activities/BaseActivity.java
+++ app/src/main/java/com/mas/sow/quranplayer/ui/activities/BaseActivity.java
@@ -1,9 +1,42 @@
 package com.mas.sow.quranplayer.ui.activities;
 
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mas.sow.quranplayer.R;
 
 /**
  * Created by Sow on 2015/03/08.
  */
 public class BaseActivity extends ActionBarActivity {
+
+    protected void setToolBar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);  
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
