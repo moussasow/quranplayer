ab5d0af1589c1dd559c21a9ab54a328526c1a752
moussasow <sow.study@gmail.com>
2015-05-04 16:05:10 +0900

    Change AppCompat version in gradle

--- app/src/main/java/com/mas/sow/quranplayer/ui/fragments/BaseFragment.java
+++ app/src/main/java/com/mas/sow/quranplayer/ui/fragments/BaseFragment.java
@@ -1,7 +1,9 @@
 package com.mas.sow.quranplayer.ui.fragments;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by Sow on 2015/03/25.
  */
-public class BaseFragment {
+public class BaseFragment extends Fragment {
 }
